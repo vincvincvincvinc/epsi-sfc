0e2119c29058c4787e31f1568d836d172c38078c
vincvincvincvinc <152969599+vincvincvincvinc@users.noreply.github.com>
2024-02-02 17:41:27 +0100

    HelloWorldOptionsApi + new page

---
+++ src/components/HelloWorldOptionsApi.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "HelloWorldOptionsApi",
+  data() {
+    return {
+      test: "ma donnée de test",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div>
+    <h1>Hello from Options API Component</h1>
+    <p>Data from test property: {{ test }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
